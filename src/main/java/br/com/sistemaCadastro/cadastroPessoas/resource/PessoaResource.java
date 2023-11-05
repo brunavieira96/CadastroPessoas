5071516cbfed011287cb6cdc6cd3409f5b7957cc
Bruna Vieira <bruna.atl@hotmail.com>
2023-11-05 06:11:18 -0300

    Finalizando resource de Pessoa

--- src/main/java/br/com/sistemaCadastro/cadastroPessoas/resource/PessoaResource.java
+++ src/main/java/br/com/sistemaCadastro/cadastroPessoas/resource/PessoaResource.java
@@ -3,12 +3,12 @@ package br.com.sistemaCadastro.cadastroPessoas.resource;
 import br.com.sistemaCadastro.cadastroPessoas.model.Pessoa;
 import br.com.sistemaCadastro.cadastroPessoas.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/pessoas")
@@ -24,10 +24,47 @@ public class PessoaResource {
     @GetMapping
     public ResponseEntity<List<Pessoa>> getAllPessoas() {
         List<Pessoa> pessoas = pessoaService.getAll();
-        if(pessoas == null) {
+        if (pessoas == null) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(pessoas);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Pessoa>> getById(@PathVariable int id) {
+        Optional<Pessoa> pessoa = pessoaService.getById(id);
+        if (pessoa == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(pessoa);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Pessoa> save(@RequestBody Pessoa pessoa) {
+        Pessoa newPessoa = pessoaService.savePessoa(pessoa);
+        if (newPessoa == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(newPessoa);
+    }
+
+    @PutMapping
+    public ResponseEntity<Pessoa> update(@RequestBody Pessoa pessoa) {
+        Pessoa newPessoa = pessoaService.update(pessoa);
+        if (newPessoa == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(newPessoa);
+
+    }
+
+    @DeleteMapping("/{/id}")
+    public ResponseEntity<?> delete(@PathVariable int id) {
+        pessoaService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
 
 }
