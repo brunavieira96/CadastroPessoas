198c3254e6ba18147f7f44d4d44a1d5ab666a568
Bruna Vieira <bruna.atl@hotmail.com>
2023-11-05 04:25:56 -0300

    Implementando a service de Pessoa

--- src/main/java/br/com/sistemaCadastro/cadastroPessoas/service/PessoaService.java
+++ src/main/java/br/com/sistemaCadastro/cadastroPessoas/service/PessoaService.java
@@ -1,8 +1,9 @@
 package br.com.sistemaCadastro.cadastroPessoas.service;
 
+import br.com.sistemaCadastro.cadastroPessoas.repository.PessoaRepository;
 import br.com.sistemaCadastro.cadastroPessoas.service.interfaces.PessoaServiceInterface;
 
-public interface PessoaService  {
+public interface PessoaService extends PessoaServiceInterface {
 
 
 }
